81e0cc062c90d731f192d0388141275ab34d0b2d
Ishwor <ishwortimalsina39@gmail.com>
2025-05-20 17:45:34 +0545

    added gitignore

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,7 +1,14 @@
 import { createClient } from "@supabase/supabase-js";
 
 export const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseKey = import.meta.env.VITE_SUPABASE_KEY;
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
-const supabase = createClient(supabaseUrl, supabaseKey);
+if (!supabaseUrl) {
+  throw new Error("supabaseUrl is required.");
+}
+if (!supabaseAnonKey) {
+  throw new Error("supabaseAnonKey is required.");
+}
+
+const supabase = createClient(supabaseUrl, supabaseAnonKey);
 export default supabase;
