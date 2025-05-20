6fb72bef1a2e6d1488490741853b5d80cea2ddac
Ishwor <ishwortimalsina39@gmail.com>
2025-05-20 17:28:20 +0545

    added gitignore

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,7 +1,7 @@
-import { createClient } from '@supabase/supabase-js';
-export const supabaseUrl = 'https://xpukqzmmtkftwruwlmqh.supabase.co';
-const supabaseKey =
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhwdWtxem1tdGtmdHdydXdsbXFoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzkyODQ3MjQsImV4cCI6MjA1NDg2MDcyNH0.IAY6Z0-9TQjiZ9ks2lYtz4CccSqf7lUMRs7rUz0pzsk';
-const supabase = createClient(supabaseUrl, supabaseKey);
+import { createClient } from "@supabase/supabase-js";
 
+export const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseKey = import.meta.env.VITE_SUPABASE_KEY;
+
+const supabase = createClient(supabaseUrl, supabaseKey);
 export default supabase;
